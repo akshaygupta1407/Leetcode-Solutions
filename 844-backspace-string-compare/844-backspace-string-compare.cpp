bfef1309936521ed103d23bd23c4b0f6c888d6ec
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-05-01 23:12:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (88.17%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,3 +1,4 @@
+//without using stack
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
@@ -31,3 +32,49 @@ public:
         return res1==res2;
     }
 };
+// using stack
+/*
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>s1;
+        for(auto x : s)
+        {
+            if(x=='#' && !s1.empty())
+            {
+                s1.pop();
+            }
+            else if(x!='#')
+            {
+                s1.push(x);
+            }
+        }
+        s = "";
+        while(!s1.empty())
+        {
+            char c = s1.top();
+            s1.pop();
+            s+=c;
+        }
+        for(auto x : t)
+        {
+            if(x=='#' && !s1.empty())
+            {
+                s1.pop();
+            }
+            else if(x!='#')
+            {
+                s1.push(x);
+            }
+        }
+        t = "";
+        while(!s1.empty())
+        {
+            char c = s1.top();
+            s1.pop();
+            t+=c;
+        }
+        return s==t;
+    }
+};
+*/
