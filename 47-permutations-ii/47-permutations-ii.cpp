b92c33d06a3673c9eb95f0c589a250c29c5add66
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-05-21 09:13:54 +0530

    Time: 116 ms (15.95%), Space: 9.8 MB (39.19%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void backtrack(vector<int>& nums, set<vector<int>>&s, vector<bool>&visited,vector<int>&sub)
+    void backtrack(vector<int>& nums,set<vector<int>>&s,vector<bool>&visited,vector<int>&sub)
     {
         if(sub.size()==nums.size())
         {
             s.insert(sub);
+            return;
         }
         for(int i=0;i<nums.size();i++)
         {
@@ -20,10 +21,10 @@ public:
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>>res;
+        vector<bool>visited(nums.size(),false);
         set<vector<int>>s;
-        vector<bool>v(nums.size(),false);
         vector<int>sub;
-        backtrack(nums,s,v,sub);
+        backtrack(nums,s,visited,sub);
         for(auto x : s)
         {
             res.push_back(x);
