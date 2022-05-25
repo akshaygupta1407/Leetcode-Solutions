97b0bdfcb4203a774f100e25f19f29b28b9efa4a
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-05-25 10:07:12 +0530

    Time: 364 ms (42.86%), Space: 10.6 MB (20.34%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int dfs(vector<int>&nums,int n)
-    {
-        int lis[n];
-        lis[0] = 1;
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,1);
         for(int i=1;i<n;i++)
         {
-            lis[i] = 1;
             for(int j=0;j<i;j++)
             {
-                if(nums[i]>nums[j] && lis[i]<lis[j]+1)
+                if(nums[i]>nums[j])
                 {
-                    lis[i] = lis[j] + 1;
+                    dp[i] = max(dp[i],dp[j]+1);
                 }
             }
         }
-        return *max_element(lis,lis+n);
-    }
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        return dfs(nums,n);
+        return *max_element(dp.begin(),dp.end());
     }
 };
