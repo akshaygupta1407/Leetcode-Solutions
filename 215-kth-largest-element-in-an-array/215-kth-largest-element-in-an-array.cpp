f31466cc84695e674a90190834d4ba2edd1ddb3f
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-05-29 14:48:41 +0530

    Time: 7 ms (88.71%), Space: 10.4 MB (14.75%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -5,9 +5,9 @@ public:
         //using maxheap
         int n = nums.size();
         int ans;
-        if(k<=n/2)
-        {
-            //perform maxheap and find kth element 
+        // if(k<=n/2)
+        // {
+        //     //perform maxheap and find kth element 
             priority_queue<int>p;
             for(int i=0;i<n;i++)
             {
@@ -18,22 +18,22 @@ public:
                 ans = p.top();
                 p.pop();
             }
-        }
-        else
-        {
-            //perform minheap and find n-kth element
-            priority_queue<int,vector<int>,greater<int>>p;
-            for(int i=0;i<n;i++)
-            {
-                p.push(nums[i]);
-            }
-            k = n-k+1;
-            while(k--)
-            {
-                ans = p.top();
-                p.pop();
-            }
-        }
+        // }
+        // else
+        // {
+        //     //perform minheap and find n-kth element
+        //     priority_queue<int,vector<int>,greater<int>>p;
+        //     for(int i=0;i<n;i++)
+        //     {
+        //         p.push(nums[i]);
+        //     }
+        //     k = n-k+1;
+        //     while(k--)
+        //     {
+        //         ans = p.top();
+        //         p.pop();
+        //     }
+        // }
         return ans;
     }
 };
