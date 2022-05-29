259762f02dd1ae09419aeb141773c710186f5a31
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-05-29 16:45:40 +0530

    Time: 516 ms (25.00%), Space: 77.4 MB (50.00%) - LeetHub

---
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    unsigned int countWordsInString(std::string const& str)
+    {
+        std::stringstream stream(str);
+        return std::distance(std::istream_iterator<std::string>(stream), std::istream_iterator<std::string>());
+    }
+    map<string,int>m1;
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        int n = messages.size();
+        for(int i=0;i<n;i++)
+        {
+            int c = countWordsInString(messages[i]);
+            m1[senders[i]]+=c;
+        }
+        string res = "";
+        int mx = 0;
+        for(auto x : m1)
+        {
+            if(x.second>=mx)
+            {
+                mx = x.second;
+                res = x.first;
+            }
+            // else if(x.second==mx)
+            // {
+            //  if(res < x.first)
+            //     {
+            //         res = x.first;
+            //     }
+            // }
+        }
+        return res;
+    }
+};
