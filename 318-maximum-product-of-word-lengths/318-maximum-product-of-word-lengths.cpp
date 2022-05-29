222d93e98858c5039d9ce6c24ec8b51f0acbcb09
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-05-29 22:20:37 +0530

    Time: 51 ms (83.16%), Space: 15.7 MB (64.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -2,25 +2,34 @@ class Solution {
 public:
     int maxProduct(vector<string>& words) {
         int n = words.size();
-        vector<int>mask(n);
-        int len = 0;
+        vector<int>mask(n);   
+        int ans = 0;
         for(int i=0;i<n;i++)
         {
             for(auto x : words[i])
             {
-                mask[i] = mask[i] | 1<<(x-'a');
+               mask[i] = mask[i] | 1 << (x - 'a'); 
             }
             for(int j=0;j<i;j++)
             {
-                if((mask[i]&mask[j])==0)
+                int a = mask[i];
+                int b = mask[j];
+                if((a&b)==0)
                 {
-                    int l1 = words[i].length();
-                    int l2 = words[j].length();
-                    int l = l1*l2;
-                    len = max(len,l);
+                    int l = words[i].length() * words[j].length();
+                    ans = max(ans,l);
                 }
             }
         }
-        return len;
+        return ans;
     }
 };
+// "abcw"
+//     a = 1
+//     b = 10
+//     c = 100
+//     w = 00010000000000000
+//     ab 11
+//     00010000000000000000000111
+// 1 << 1          10
+// 1<<2            100000
