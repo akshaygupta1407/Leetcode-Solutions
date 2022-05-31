12840959393dd6373dac33d96cbe65d2e5b757ae
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-05-31 15:47:36 +0530

    Time: 33 ms (41.11%), Space: 15.6 MB (15.84%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool cycle(unordered_map<int,list<int>>&h,vector<int>&visited,int src)
+    {
+        if(visited[src]==2)
+            return true;
+        visited[src] = 2;
+        for(auto nbr : h[src])
+        {
+            if(visited[nbr]!=1)
+            {
+                if(cycle(h,visited,nbr))
+                    return true;
+            }
+        }
+        visited[src] = 1;
+        return false;
+        
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,list<int>>h;
+        vector<int>visited(numCourses,0);
+        for(auto x : prerequisites)
+        {
+            int u = x[0],v = x[1];
+            h[u].push_back(v);
+        }
+        for(int i=0;i<numCourses;i++)
+        {
+                if(visited[i]==0)
+                {
+                    if(cycle(h,visited,i))
+                        return false;
+                }
+        }
+        return true;
+    }
+};
