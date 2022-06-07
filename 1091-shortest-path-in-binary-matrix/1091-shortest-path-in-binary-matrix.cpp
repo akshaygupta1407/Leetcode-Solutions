bcfae0f780be8e84b22bc5a37044996141800881
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-06-07 23:08:16 +0530

    Time: 516 ms (7.17%), Space: 75.1 MB (5.98%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0]==1 || grid[n-1][n-1]==1)  return -1;
-        vector<vector<int>>directions = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
+        if(grid[0][0]==1 || grid[n-1][n-1]==1)
+            return -1;
+        vector<vector<int>>dir = {{1,0},{0,1},{0,-1},{-1,0},{1,1},{-1,-1},{1,-1},{-1,1}};;
         queue<pair<int,int>>q;
         q.push({0,0});
         grid[0][0] = 1;
         while(!q.empty())
         {
-            auto curr = q.front();
+            auto node = q.front();
             q.pop();
-            int x = curr.first;
-            int y = curr.second;
+            int x = node.first, y = node.second;
             if(x==n-1 && y==n-1)
                 return grid[x][y];
-            for(auto dir : directions)
+            for(auto d : dir)
             {
-                int currx = x + dir[0];
-                int curry = y + dir[1];
+                int currx = x + d[0];
+                int curry = y + d[1];
                 if(currx>=0 && curry>=0 && currx<n && curry<n && grid[currx][curry]==0)
                 {
-                    grid[currx][curry] = grid[x][y]+1;
+                    grid[currx][curry] = grid[x][y] + 1;
                     q.push({currx,curry});
                 }
             }
