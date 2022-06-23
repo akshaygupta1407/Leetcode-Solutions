56dfeba0cadbe544cea0c00fdd8dc316c0f6b9c2
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-06-23 17:25:06 +0530

    Added solution - LeetHub

---
+++ Plus One - GFG/plus-one.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> increment(vector<int> digits ,int N) {
+        // code here
+        bool carry = true;
+        
+        for(int i=N-1; i >= 0 && carry; i--) {
+            carry = (++digits[i]%=10) == 0;
+        }
+
+        if(carry) {
+            digits.insert(digits.begin(), 1);
+        }
+    
+        return digits;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        char c;
+        
+        cin>>N;
+        vector<int> arr(N);
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        vector<int> res = ob.increment(arr,N);
+        for(int i: res)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
