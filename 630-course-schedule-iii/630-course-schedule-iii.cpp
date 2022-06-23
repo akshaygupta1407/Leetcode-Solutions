1aca7c96ffd0be83fbad661b12413fe20415211a
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-06-23 08:54:46 +0530

    Time: 2060 ms (5.09%), Space: 265.3 MB (5.74%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,69 @@
+// class Solution {
+// public:
+//     static bool cmp(vector<int>a,vector<int>b)
+//     {
+//         return a[1] < b[1];
+//     }
+//     int scheduleCourse(vector<vector<int>>& courses) {
+//         int n = courses.size();
+//         if(courses.size() ==0) return 0;
+//         sort(courses.begin(),courses.end(),cmp);
+//         priority_queue<int>q;
+//         int time = 0;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(time + courses[i][0] <= courses[i][1])
+//             {
+//                 time+=courses[i][0];
+//                 q.push(courses[i][0]);
+//             }
+//             else
+//             {
+//                 if(!q.empty() && q.top() > courses[1][0])
+//                 {
+//                     int x = q.top();
+//                     q.pop();
+//                     time-=x;
+//                     time+=courses[i][0];
+//                     q.push(courses[i][0]);
+//                 }
+//             }
+//         }
+//         return q.size();
+//     }
+// };  
+class Solution {
+public:
+    static bool cmp(vector<int> a, vector<int> b) {
+        return a[1]<b[1];
+    }
+    
+    int scheduleCourse(vector<vector<int>>& courses) {
+        
+        int n = courses.size();
+        int time = 0;
+        priority_queue<int> q;
+        
+        sort(courses.begin(), courses.end(), cmp);
+            
+        for(int i=0; i<n; i++) {
+            if(time+courses[i][0] <= courses[i][1]) {
+                // we can take this course
+                time+=courses[i][0];
+                q.push(courses[i][0]);
+            } else {
+                if(!q.empty() && q.top() > courses[i][0]) {
+                    // current course is actually of 
+                    //less duration and can be taken
+                    int x = q.top();
+                    q.pop();
+                    time-=x;
+                    time+=courses[i][0];
+                    q.push(courses[i][0]);
+                }
+            }
+        }
+        
+        return q.size();
+    }
+};
