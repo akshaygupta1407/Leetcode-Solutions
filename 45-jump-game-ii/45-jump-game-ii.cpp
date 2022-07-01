fc996262c6dd1825ca4b7551f46ce1485853a1dd
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-07-01 13:21:49 +0530

    Time: 15 ms (91.94%), Space: 16.6 MB (81.09%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -4,17 +4,17 @@ public:
         int n = nums.size();
         if(n==1)    return 0;
         if(nums[0]==0)  return -1;
-        int maxreach = nums[0],steps = nums[0],jump = 1;
+        int mxreach = nums[0], steps = nums[0],jump = 1;
         for(int i=1;i<n;i++)
         {
             if(i==n-1)  return jump;
-            maxreach = max(maxreach,i+nums[i]);
+            mxreach = max(mxreach,i+nums[i]);
             steps--;
             if(steps==0)
             {
                 jump++;
-                if(i>=maxreach) return -1;
-                steps = maxreach-i;
+                if(i>=mxreach)  return -1;
+                steps = mxreach - i;
             }
         }
         return -1;
