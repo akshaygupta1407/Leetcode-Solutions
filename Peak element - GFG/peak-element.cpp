eca93f2b3ba3d475a72313175325603ed44b3e33
Akshay Gupta <85879402+akshaygupta1407@users.noreply.github.com>
2022-07-05 21:34:57 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.cpp
+++ Peak element - GFG/peak-element.cpp
@@ -15,14 +15,30 @@ class Solution
     int peakElement(int arr[], int n)
     {
        // Your code here
-       if(n==1) return 0;
-       if(arr[0]>arr[1])    return 0;
-       if(arr[n-1]>arr[n-2])    return n-1;
-       for(int i=1;i<n-2;i++)
-       {
-           if(arr[i]>arr[i-1] && arr[i]> arr[i+1])  return i;
-       }
-       return 0;
+       int l = 0;
+    int r = n-1;
+    int mid;
+  
+    while (l <= r) {
+      
+        // finding mid by binary right shifting.
+        mid = (l + r) >> 1;
+      
+        // first case if mid is the answer
+        if ((mid == 0 || arr[mid - 1] <= arr[mid])
+            and (mid == n - 1 || arr[mid + 1] <= arr[mid]))
+            break;
+      
+        // move the right pointer
+        if (mid > 0 and arr[mid - 1] > arr[mid])
+            r = mid - 1;
+      
+        // move the left pointer
+        else
+            l = mid + 1;
+    }
+  
+    return mid;
     }
 };
 
