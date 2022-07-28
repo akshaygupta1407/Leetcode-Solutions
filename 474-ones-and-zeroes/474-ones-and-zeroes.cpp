479ae7155b63ef9f0cb1e7a125841bfd39c5350e
Akshay Gupta <85879402+akshaygupta1407@users.noreply.github.com>
2022-07-28 09:20:12 +0530

    Time: 816 ms (11.27%), Space: 35.9 MB (37.36%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int dp[101][101][601];
-    int helper(vector<string>& strs, int m, int n,int index)
+    int helper(vector<string>& strs, int m, int n,int idx)
     {
-        if(index>=strs.size() || (m==0 && n==0))    //m->zeroes, n->ones
+        if(idx>=strs.size() || (m==0 && n==0))    return 0;
+        int count0=0,count1=0;
+        if(dp[m][n][idx]>0) return dp[m][n][idx];
+        for(auto x : strs[idx])
         {
-            return 0;
+            if(x=='0')  count0++;
+            else    count1++;
         }
-        if(dp[m][n][index]>0)   return dp[m][n][index];
-        int zero=0,one = 0;
-        for(auto x : strs[index])
-        {
-            if(x=='0')zero++;
-            else one++;
-        }
-        int consider = 0,skip = 0;
-        if(m>=zero && n>=one)
-        {
-            consider = 1 + helper(strs,m-zero,n-one,index+1);
-        }
-        else
-        {
-            consider = 0;
-        }
-        skip = helper(strs,m,n,index+1);
-        return dp[m][n][index] = max(skip,consider);
+        int consider = (m>=count0 && n>=count1) ? 1 + helper(strs,m-count0,n-count1,idx+1) : 0;
+        int skip = helper(strs,m,n,idx+1);
+        return dp[m][n][idx] = max(skip,consider);
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
         return helper(strs,m,n,0);
