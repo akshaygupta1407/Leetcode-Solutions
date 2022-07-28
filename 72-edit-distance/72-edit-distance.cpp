29f493ee0ea26fbd2e894fe34936d45b98b0a575
Akshay Gupta <85879402+akshaygupta1407@users.noreply.github.com>
2022-07-28 09:34:24 +0530

    Time: 139 ms (12.12%), Space: 80.7 MB (9.23%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int helper(string word1, string word2,int m,int n,vector<vector<int>>&dp)
+    int helper(string word1,string word2,int m,int n,vector<vector<int>>&dp)
     {
-        if(m==0)    return n;
-        if(n==0)    return m;
+        if(m==0) return n;
+        if(n==0) return m;
         if(dp[m][n]!=-1)    return dp[m][n];
-        if(word1[m-1]==word2[n-1])
-        {
-            return dp[m][n] = helper(word1,word2,m-1,n-1,dp);
-        }
+        if(word1[m-1]==word2[n-1])  return dp[m][n] = helper(word1,word2,m-1,n-1,dp);
         else
         {
-            int insert,del,replace;
-            insert = helper(word1,word2,m,n-1,dp);
-            del = helper(word1,word2,m-1,n,dp);
-            replace = helper(word1,word2,m-1,n-1,dp);
-            return dp[m][n] = 1 + min(insert,min(del,replace));
+            int ins,dlt,rep;
+            ins = helper(word1,word2,m,n-1,dp);
+            dlt = helper(word1,word2,m-1,n,dp);
+            rep = helper(word1,word2,m-1,n-1,dp);
+            return dp[m][n]  =1  + min(ins,min(dlt,rep));
         }
     }
     int minDistance(string word1, string word2) {
