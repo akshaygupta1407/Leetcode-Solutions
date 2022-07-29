5b6a47401c7af8638650dbc0db7d4d670058d759
Akshay Gupta <85879402+akshaygupta1407@users.noreply.github.com>
2022-07-29 19:32:17 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -9,14 +9,14 @@ public:
         }
         triangle[1].push_back(1);
         triangle[1].push_back(1);
-        if(numRows<=2)
+        if(numRows==2)
         {
             return triangle;
         }
-        for(int i = 2;i<numRows;i++)
+        for(int i=2;i<numRows;i++)
         {
             triangle[i].resize(i+1);
-            triangle[i][0] = triangle[i][i] = 1;
+            triangle[i][0] = 1,triangle[i][i] = 1;
             for(int j=1;j<i;j++)
             {
                 triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
