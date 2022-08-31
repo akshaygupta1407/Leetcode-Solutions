f87ecc79bf03128f53f745c8bf7354ba74a5f4ea
Akshay Gupta <85879402+akshaygupta1407@users.noreply.github.com>
2022-08-31 12:51:42 +0530

    Time: 225 ms (55.39%), Space: 28.4 MB (13.95%) - LeetHub

---
+++ 170-two-sum-iii-data-structure-design/170-two-sum-iii-data-structure-design.cpp
@@ -0,0 +1,34 @@
+class TwoSum {
+public:
+    map<long long,long long>mpp;
+    TwoSum() {
+        mpp.clear();
+    }
+    
+    void add(int number) {
+        mpp[number]++;
+    }
+    
+    bool find(int value) {
+        for(auto x : mpp)
+        {
+            if(mpp.find(value-x.first)!=mpp.end())
+            {
+                if(value-x.first==x.first && mpp[x.first]>=2)
+                return true;
+                if(value-x.first!=x.first)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum* obj = new TwoSum();
+ * obj->add(number);
+ * bool param_2 = obj->find(value);
+ */
