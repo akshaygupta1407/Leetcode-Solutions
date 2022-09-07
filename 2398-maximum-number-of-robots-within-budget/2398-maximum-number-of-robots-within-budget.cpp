e1c621cec79fecbbfbff5f39e67ef559b95c0420
Akshay Gupta <85879402+akshaygupta1407@users.noreply.github.com>
2022-09-07 22:00:32 +0530

    Time: 357 ms (91.19%), Space: 116.8 MB (78.78%) - LeetHub

---
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
+        long long n = chargeTimes.size();
+        long long mx = 0;
+        vector<long long> runningCost(n,0);
+        runningCost[0] = runningCosts[0];
+        for(long long i=1;i<n;i++)
+        {
+            runningCost[i]=runningCost[i-1]+ runningCosts[i];
+        }
+        deque<long long>dq;
+        long long i=0,j=0;
+        while(i<n)
+        {
+            if(dq.empty())
+            {
+                dq.push_back(chargeTimes[i]);
+            }
+            long long x = dq.front();
+            long long y = (long long)(abs(i-j)+1)*(j==0 ? runningCost[i] : runningCost[i] - runningCost[j-1]);
+            long long check = (long long)((long long)x+(long long)y);;
+            if(check <= budget)
+            {
+                mx = max(mx,abs(i-j+1));
+                i++;
+                if(i<n && (dq.back() < chargeTimes[i]))
+                {
+                    while(!dq.empty() && dq.back() < chargeTimes[i])
+                    dq.pop_back();
+                    
+                    dq.push_back(chargeTimes[i]);
+                }
+                else if(i<n && (dq.back() >= chargeTimes[i]))
+                {
+                    dq.push_back(chargeTimes[i]);
+                }
+            }
+            else
+            {
+                if(j<n && dq.front()==chargeTimes[j])
+                {
+                    dq.pop_front();
+                }
+                j++;
+                if(i<j)
+                {
+                    i++;
+                }
+            }
+        }
+        return mx;
+    }
+};
