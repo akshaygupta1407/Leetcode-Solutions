858ea7f8e06d9f07e8eef437b82545e20d8d82c3
Akshay Gupta <85879402+akshaygupta1407@users.noreply.github.com>
2022-10-16 20:06:24 +0530

    Time: 4 ms (33.87%), Space: 6.4 MB (50.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stack;
+        for(auto x : s)
+        {
+            if(stack.empty() || x=='(' || x=='[' || x=='{')
+            {
+                stack.push(x);
+            }
+            else
+            {
+                if((x==')' && stack.top()=='(') || (x=='}' && stack.top()=='{') || (x==']' && stack.top()=='['))
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+        }
+        if(!stack.empty())  return false;
+        return true;
+    }
+};
